47d0dfc9dd1c07814e23dd2243b8898b00853a74
Hemlalit Mali <149302213+hemlalit@users.noreply.github.com>
2025-07-17 03:10:58 +0530

    Update pneumonia (.keras) detection.py

--- pneumonia detection.py
+++ pneumonia detection.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Load model
-model = load_model('pnuemonia pred model.h5')
+model = load_model('pnuemonia pred model.keras')
 
 # Class labels
 class_labels = ['Normal', 'Pneumonia']
